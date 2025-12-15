03c3195396b0d54ba5631136c085279e924614e8
Ajmal <ajmalsadhath@gmail.com>
2025-12-15 11:49:58 +0530

    implement state mgt with zestund

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { Tooltip } from 'react-tooltip'
 import {dockApps} from "#constants/index.js";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "#store/window.js";
 
 const Dock = () => {
+    const {openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(()=> {
@@ -56,8 +58,20 @@ const Dock = () => {
 
 
     const toggleApp = (app) => {
+        if(!app.canOpen) return;
 
-    }
+        const window = windows[app.id];
+
+        if (!window) {
+            console.error(`window not found: ${app.id}`);
+            return;
+        }
+        if(window.isOpen) {
+            closeWindow(app.id);
+        }else {
+            openWindow(app.id);
+        }
+    };
 
     return <section id="dock">
         <div ref = {dockRef} className="dock-container">
